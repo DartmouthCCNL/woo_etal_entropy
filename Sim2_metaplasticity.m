4dfb85736bd405a6398fb4f4767a4b8561f79cac
DartmouthCCNL <69058564+DartmouthCCNL@users.noreply.github.com>
2025-05-22 15:10:59 -0400

    Update Sim2_metaplasticity.m

--- Sim2_metaplasticity.m
+++ Sim2_metaplasticity.m
@@ -21,7 +21,6 @@ rev_pos = 40;
 RewProbs = Construct_reward_schedule(rew_probs, blockL, rev_pos);
 EnvLbl = max(RewProbs(1,:))*100+"/"+min(RewProbs(1,:))*100+"%";
 
-sem = @(x,DIM) std(x,[],DIM,'omitnan')./sqrt(sum(~isnan(x),DIM));
 FontS = 15;
 
 %% 1. Plot simulated signals: comaprison with plastic RL
